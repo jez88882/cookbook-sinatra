8e293e5b9eb3cd51bdedf6cbe9f2d40cdb0b8924
jiaen zhang <jiaen.zhang98@gmail.com>
2021-11-01 16:14:35 +0800

    Boilerplate sinatra app

---
+++ app.rb
@@ -0,0 +1,13 @@
+ #Sinatra app main file, the entry point, the one we want to run
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  'Hello me!'
+end
